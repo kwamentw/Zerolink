6643fa2e0144ec81a29bfc456ce8b7cdf27b4456
kwamentw <antwi1496@hotmail.com>
2025-06-05 19:06:06 -0400

    +insurance contract

---
+++ src/Insurance.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+/**
+ * @title Insurance Payments
+ * @author 4B
+ * @notice Please this is not meant for production
+ */
+contract Insurance{
+    
+}
--- src/ZeroLink.sol
+++ src/ZeroLink.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.28;
  * @title Zero Link
  * @author 4b
  * @notice A contract to learn how to integrate layerzero
- * PLEASE DO NOT USE AS PRODUCTION CODE
+ * PLEASE DO NOT USE AT PRODUCTION
  */
 contract ZeroLink {
 
