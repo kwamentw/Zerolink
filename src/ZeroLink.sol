2f799f024330eaff2ecc4e4924f65c17cd91f9b2
kwamentw <antwi1496@hotmail.com>
2025-06-04 20:58:41 -0400

    +README

--- src/ZeroLink.sol
+++ src/ZeroLink.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+/**
+ * @title Zero Link
+ * @author 4b
+ * @notice A contract to learn how to integrate layerzero
+ * PLEASE DO NOT USE AS PRODUCTION CODE
+ */
 contract ZeroLink {
 
 }
