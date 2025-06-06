089482683d2ec2cad1c58585d08b391c3984264c
kwamentw <antwi1496@hotmail.com>
2025-06-06 19:14:14 -0400

    +insurance contract

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+//import layer zero interfaces
+
 /**
  * @title Insurance Payments
  * @author 4B
  * @notice Please this is not meant for production
  */
 contract Insurance{
+    function createPollicy() external {}
+    function depositPayment() external{}
+    function processPayout() external {}
+    function submitClaim() external{}
+    function approveClaim() external {}
+
     
 }
