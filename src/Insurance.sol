643ff3a2522e0bbd6ce942bc55f0400121c23eb4
kwamentw <antwi1496@hotmail.com>
2025-06-10 19:59:59 -0400

    +create Policy

--- src/Insurance.sol
+++ src/Insurance.sol
@@ -9,8 +9,43 @@ pragma solidity 0.8.28;
  * @notice Please this is not meant for production
  */
 contract Insurance{
+
+    error PolicyExists();
+    event PolicyCreated(address polcyHolder, uint256 expiration);
+
+    struct Policy {
+        address policyHolder;
+        uint256 coverageLimitAmt;
+        uint256 premiumAmtToPay;
+        uint256 expiration;
+        uint256 payCounter;
+        address payoutReceiver;
+    }
+
+    // one that creates the policy should be different from the user, the user should only make payments
+
+    mapping (address _policyHolder => Policy holdersPolicy) public policies;
+
     // create insurance policy with necessary params
-    function createPollicy() external {}
+    function createPolicy(Policy memory newPolicy) external payable{
+
+        address policyHolder = newPolicy.policyHolder;
+
+        require(newPolicy.expiration > block.timestamp, "expiration already passed");
+        require(newPolicy.coverageLimitAmt > 0 && newPolicy.premiumAmtToPay > 0, "invalid coverage Amount & premiumAmt");
+        require(newPolicy.payoutReceiver != address(0), "invalid receiver");
+
+        if(policyHolder != address(0)){
+            policies[policyHolder] = newPolicy;
+            emit PolicyCreated(policyHolder, newPolicy.expiration);
+        } else {
+            revert PolicyExists();
+        }
+    }
+    // to update insurance policy
+    function updatePolicy() public {}
+    // deletes the policy
+    function terminatePolicy() external {}
     // logic to make insurance payments
     function depositPayment() external{}
     // processing payout
@@ -20,6 +55,3 @@ contract Insurance{
     // Insurance body approves claim
     function approveClaim() external {}    
 }
-//////////////////////////////////////////
-//////////////////////////////////////////
-// Im feeling lazy! I have to work 
