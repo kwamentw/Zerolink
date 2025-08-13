344ee3291338f3c2c8883e29a3697fa6a9d804ab
kwamentw <antwi1496@hotmail.com>
2025-08-12 20:41:01 -0400

    cooking

--- src/Lending.sol
+++ src/Lending.sol
@@ -15,12 +15,12 @@ contract Lending{
  * @author 4b
  * @notice Meant for testing whether a scanner works well
  */
-contract testScanner{
+contract testScanner1{
     address owner;
     address pendingOwner;
 
     event OwnerSuggested();
-    event OwnerAccepted();
+    event OwnerAccepted(address oldOwner, address newOwner);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "error");
@@ -32,27 +32,28 @@ contract testScanner{
     }
 
     function suggestOwner(address newOwner) external onlyOwner{
+        require(pendingOwner == address(0), "set already");
         require(newOwner != address(0), "not a valid address");
         pendingOwner = newOwner;
         emit OwnerSuggested();
     }
-    /**
-     * delete current owner
-     * check whether there is a pendingowner otherwise reject
-     */
+
+
     function renounceOwnership() internal{
+        require(pendingOwner != address(0), "invalid");
         delete owner;
     }
 
     function acceptOwner() external {
         require(msg.sender == pendingOwner,"not allowed");
         renounceOwnership();
+        address oldOwner = owner;
         owner = pendingOwner;
         pendingOwner = address(0);
 
-        emit OwnerAccepted();
+        emit OwnerAccepted(oldOwner, owner);
 
-    } // add emit old and new owners
+    }
 
     // try role base type too
     
